e76572e2976aebaebc85c1bd8c735b6e523604a1
Seokju Hong <iamseokjuhong@gmail.com>
2025-07-01 13:00:01 +0900

    10. 리팩토링 5. 코드 정리하기 complete

--- src/main/java/me/whiteship/refactoring/_02_duplicated_code/practice/_05_slide_statements/StudyDashboard.java
+++ src/main/java/me/whiteship/refactoring/_02_duplicated_code/practice/_05_slide_statements/StudyDashboard.java
@@ -26,12 +26,12 @@ public class StudyDashboard {
 
     private void printReviewers() throws IOException {
         // Get github issue to check homework
-        Set<String> reviewers = new HashSet<>();
         GitHub gitHub = GitHub.connect();
         GHRepository repository = gitHub.getRepository("whiteship/live-study");
         GHIssue issue = repository.getIssue(30);
 
         // Get reviewers
+        Set<String> reviewers = new HashSet<>();
         issue.getComments().forEach(c -> reviewers.add(c.getUserName()));
 
         // Print reviewers
