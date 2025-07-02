b50654814429476797d2398c86462d769f70e634
Seokju Hong <iamseokjuhong@gmail.com>
2025-07-02 12:17:27 +0900

    21. 리팩토링 14. 매개변수를 질의 함수로 바꾸기 init

---
+++ src/test/java/me/whiteship/refactoring/_04_long_parameter_list/practice/_14_replace_parameter_with_query/OrderTest.java
@@ -0,0 +1,24 @@
+package me.whiteship.refactoring._04_long_parameter_list.practice._14_replace_parameter_with_query;
+
+import me.whiteship.refactoring._04_long_parameter_list._14_replace_parameter_with_query.Order;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class OrderTest {
+
+    @Test
+    void discountedPriceWithDiscountLevel2() {
+        int quantity = 200;
+        double price = 100;
+        assertEquals(quantity * price * 0.90, new me.whiteship.refactoring._04_long_parameter_list._14_replace_parameter_with_query.Order(quantity, price).finalPrice());
+    }
+
+    @Test
+    void discountedPriceWithDiscountLevel1() {
+        int quantity = 100;
+        double price = 100;
+        assertEquals(quantity * price * 0.95, new Order(quantity, price).finalPrice());
+    }
+
+}
