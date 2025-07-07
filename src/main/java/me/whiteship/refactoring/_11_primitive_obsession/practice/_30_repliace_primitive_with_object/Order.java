b0ea911398e1c58aea7de665318c992ad5fbb671
Seokju Hong <iamseokjuhong@gmail.com>
2025-07-07 12:50:20 +0900

    44. 리팩토링 30. 기본형을 객체로 바꾸기 complete

--- src/main/java/me/whiteship/refactoring/_11_primitive_obsession/practice/_30_repliace_primitive_with_object/Order.java
+++ src/main/java/me/whiteship/refactoring/_11_primitive_obsession/practice/_30_repliace_primitive_with_object/Order.java
@@ -2,13 +2,17 @@ package me.whiteship.refactoring._11_primitive_obsession.practice._30_repliace_p
 
 public class Order {
 
-    private String priority;
+    private Priority priority;
 
-    public Order(String priority) {
+    public Order(String priorityValue) {
+        this(new Priority(priorityValue));
+    }
+
+    public Order(Priority priority) {
         this.priority = priority;
     }
 
-    public String getPriority() {
+    public Priority getPriority() {
         return priority;
     }
 }
