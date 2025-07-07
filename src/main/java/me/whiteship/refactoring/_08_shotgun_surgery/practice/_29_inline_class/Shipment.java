5a53f5993e38129357ca685a4c76335e34297ddf
Seokju Hong <iamseokjuhong@gmail.com>
2025-07-07 12:04:13 +0900

    40. 리팩토링 29. 클래스 인라인 complete
    옮길 클래스의 필드를 먼저 옮긴 후 메소드를 옮기는 것이 좀 더 괜찮은듯

--- src/main/java/me/whiteship/refactoring/_08_shotgun_surgery/practice/_29_inline_class/Shipment.java
+++ src/main/java/me/whiteship/refactoring/_08_shotgun_surgery/practice/_29_inline_class/Shipment.java
@@ -1,22 +1,33 @@
 package me.whiteship.refactoring._08_shotgun_surgery.practice._29_inline_class;
 
 public class Shipment {
+    
+    private String shippingCompany;
 
-    private TrackingInformation trackingInformation;
-
-    public Shipment(TrackingInformation trackingInformation) {
-        this.trackingInformation = trackingInformation;
-    }
-
-    public TrackingInformation getTrackingInformation() {
-        return trackingInformation;
-    }
-
-    public void setTrackingInformation(TrackingInformation trackingInformation) {
-        this.trackingInformation = trackingInformation;
+    private String trackingNumber;
+    
+    public Shipment(String shippingCompany, String trackingNumber) {
+        this.shippingCompany = shippingCompany;
+        this.trackingNumber = trackingNumber;
     }
 
     public String getTrackingInfo() {
-        return this.trackingInformation.display();
+        return this.shippingCompany + ": " + this.trackingNumber;
+    }
+
+    public String getShippingCompany() {
+        return shippingCompany;
+    }
+
+    public void setShippingCompany(String shippingCompany) {
+        this.shippingCompany = shippingCompany;
+    }
+
+    public String getTrackingNumber() {
+        return trackingNumber;
+    }
+
+    public void setTrackingNumber(String trackingNumber) {
+        this.trackingNumber = trackingNumber;
     }
 }
