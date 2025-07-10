3401e196af6a23afec545313781a06ebdb77dfe0
somefood <iamseokjuhong@gmail.com>
2025-07-10 23:27:12 +0900

    65. 냄새 21. 서로 다른 인터페이스의 대안 클래스들 complete

--- src/main/java/me/whiteship/refactoring/_21_alternative_classes_with_different_interfaces/practice/OrderAlerts.java
+++ src/main/java/me/whiteship/refactoring/_21_alternative_classes_with_different_interfaces/practice/OrderAlerts.java
@@ -2,12 +2,16 @@ package me.whiteship.refactoring._21_alternative_classes_with_different_interfac
 
 public class OrderAlerts {
 
-    private AlertService alertService;
-    
-    public void alertShipped(Order order) {
-        AlertMessage alertMessage = new AlertMessage();
-        alertMessage.setMessage(order.toString() + " is shipped");
-        alertMessage.setFor(order.getEmail());
-        alertService.add(alertMessage);
+    private final NotificationService notificationService;
+
+    public OrderAlerts(NotificationService notificationService) {
+        this.notificationService = notificationService;
     }
+
+    public void alertShipped(Order order) {
+        Notification notification = Notification.newNotification(order.toString() + " is shipped")
+                .receiver(order.getEmail());
+        notificationService.sendNotification(notification);
+    }
+
 }
